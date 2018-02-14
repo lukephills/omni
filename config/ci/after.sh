365d90fba266cf69eaeb67bc929edfff5338cef8
Luke Phillips <luke@femurdesign.com>
2018-02-14 14:45:39 +0000

    simplified build process, updated to latest node

--- config/ci/after.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex -o pipefail
-
-# Upload to coveralls, but don't _fail_ if coveralls is down.
-cat coverage/lcov.info | node_modules/.bin/coveralls || echo "Coveralls upload failed"
-node node_modules/lcov-filter/index.js lcov.info config | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
-
